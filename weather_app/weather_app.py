be8710ab3ae965fef3be525ad2cff77e8625cd8f
Rharsia <Jan.Zhang@seznam.cz>
2023-04-10 01:12:29 +0200

    Finalized Weather App

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -8,32 +8,44 @@ import requests
 api_key = "f863f2b4679723f139f41d5e8b55948f"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-# prompt the user for the city name
-city = input("Enter city name: ")
+while True:
+    # prompt the user for the city name
+    city = input("Enter city name: ")
 
-# Create the URL for the API request
-url = f"{base_url}q={city}&appid={api_key}"
+    # Create the URL for the API request
+    url = f"{base_url}q={city}&appid={api_key}"
 
-print(url)
+    print(url)
 
-# send the API request and get the response
-response = requests.get(url)
+    # send the API request and get the response
+    response = requests.get(url)
 
-# parse the JSON data from the response
-data = response.json()
+    # parse the JSON data from the response
+    data = response.json()
 
-# check if the API returne an error
-if data["cod"] != "404":
-    # Extract the weather information from the JSON data
-    weather = data["weather"][0]["description"]
-    temperature = data["main"]["temp"]
-    humidity = data["main"]["humidity"]
+    # check if the API returne an error
+    if data["cod"] != "404":
+        # Extract the weather information from the JSON data
+        weather = data["weather"][0]["description"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
 
-    # Display the weather information to the user
-    print(f"Current weather in {city}:")
-    print(f"Description: {weather}")
-    print(f"Temperature: {temperature} K")
-    print(f"Humidity: {humidity}%")
+        # Display the weather information to the user
+        print(f"Current weather in {city}:")
+        print(f"Description: {weather}")
+        print(f"Temperature: {round(temperature - 273.15, 2)} Celsium")
+        print(f"Humidity: {humidity}%")
 
-else:
-    print("City not found.")
+    else:
+        print("City not found.")
+
+    
+    print()
+    print("Continue?")
+    answer = input("(y/n): ")
+    print()
+
+    if answer == "y":
+        continue
+    else:
+        break
