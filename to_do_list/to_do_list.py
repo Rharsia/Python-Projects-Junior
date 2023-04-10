48e6cb36b178d9180c66ab5e18996543401157ba
Rharsia <Jan.Zhang@seznam.cz>
2023-04-10 12:23:50 +0200

    Basic ToDo List

--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -6,6 +6,7 @@ def add_task():
     task = input("Enter task: ")
     tasks.append(task)
     print("Task added successfully.")
+    print()
 
 # create a function that deletes tasks
 def delete_task():
@@ -16,18 +17,37 @@ def delete_task():
         print("Task deleted successfully.")
     else:
         print("Task not found.")
+    print()
 
 # function to view tasks
 def view_tasks():
     print("Task list: ")
     order = 1
     for task in tasks:
-        print(f"{order}. {task}")
+        print(f"{order}) {task}")
         order += 1
+    print()
+    
+# function to show the menu
+def show_menu():
+    print("Menu:")
+    print("1. Add task")
+    print("2. Delete task")
+    print("3. View tasks")
+    print("4. Quit")
+    print()
 
-
-
-add_task()
-delete_task()
-view_tasks()
-tasks
+# display the menu and handle user input
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_task()
+    elif choice == "2":
+        delete_task()
+    elif choice == "3":
+        view_tasks()
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice. Please try again.")
