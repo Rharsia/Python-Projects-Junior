c5e569cad8941b45e61715b089947eaa53ab3e56
Rharsia <Jan.Zhang@seznam.cz>
2023-04-11 19:14:33 +0200

    Created an app that'll turn of (sleep) PC

---
+++ pc_timer/sleep_timer.py
@@ -0,0 +1,30 @@
+import time
+import os
+
+print("This is a timer to turn off (sleep) the PC")
+print()
+
+# get the number of minutes from the user
+# minutes = float(input("Enter the number of minutes: "))
+minutes = 0.5
+
+# convert to seconds
+seconds = round(minutes * 60)
+
+# display the message
+print(f"The computer will sleep in {minutes} minutes.")
+
+# loop through the countdown timer
+for i in range(seconds, 0, -1):
+    # calculate the minutes and seconds remaining
+    mins, secs = divmod(i, 60)
+
+    # display the remaining time
+    print(f"{mins:02d}:{secs:02d}", end="\r")
+
+    # wait for one second
+    time.sleep(1)
+
+print("SHUTDOWN")
+
+# os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
