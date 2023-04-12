3606aeae22a47f19d17505747df0d98e6a62fb00
Rharsia <Jan.Zhang@seznam.cz>
2023-04-12 22:23:51 +0200

    Almost finished

---
+++ rock_paper_scissor_game/Rock_paper_scissor.py
@@ -0,0 +1,77 @@
+import random
+
+def menu():
+    print("Options:")
+    print("1) Rock")
+    print("2) Paper")
+    print("3) Scissors")
+
+def choice():
+    menu()
+    print()
+    choice = int(input("Enter your choice (1-3): "))
+    
+    if choice == 1:
+        player = "rock"
+    elif choice == 2:
+        player = "paper"
+    elif choice == 3:
+        player = "scissor"
+
+    return player
+
+def evaluation(player_choice):
+    options = ["rock", "paper", "scissor"]
+    pc_choice = random.choice(options)
+
+    if pc_choice == "rock":
+        if player_choice == "rock":
+            result = "draw"
+        elif player_choice == "paper":
+            result = "win"
+        elif player_choice == "scissor":
+            result = "lose"
+    elif pc_choice == "paper":
+        if player_choice == "rock":
+            result = "lose"
+        elif player_choice == "paper":
+            result = "draw"
+        elif player_choice == "scissor":
+            result = "win"
+    elif pc_choice == "scissor":
+        if player_choice == "rock":
+            result = "win"
+        elif player_choice == "paper":
+            result = "lose"
+        elif player_choice == "scissor":
+            result = "draw"
+
+    return (result, player_choice, pc_choice)
+
+
+def game():
+    no_wins = 0
+    no_losses = 0
+
+    while True:
+        player_choice = choice()
+        print()
+
+        result = evaluation(player_choice)
+
+        if result[0] == "win":
+            print("Win!")
+            no_wins += 1
+        elif result[0] == "draw":
+            print("Draw!")
+        elif result[0] == "lose":
+            print("Loss!")
+            no_losses += 1
+        print(f"You played {result[1]}, pc played {result[2]}")
+        
+
+        print(f"Wins: {no_wins}; Losses: {no_losses}")
+
+game()
+
+
