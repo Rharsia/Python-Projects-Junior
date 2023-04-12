3d3ae26eb624b2cfb838fa29ae7874d9ddb375ad
Rharsia <Jan.Zhang@seznam.cz>
2023-04-12 15:40:06 +0200

    Finished Dice_Rolling simulator

--- dice_rolling_simulator/dice_rolling_simulator.py
+++ dice_rolling_simulator/dice_rolling_simulator.py
@@ -1,1 +1,21 @@
+import random
+
 # single dice roll
+# result = random.randint(1,6)
+# result
+
+# any number of dices
+n_dices = int(input("Number of dices to roll: "))
+
+def dice_roll(n_dices):
+    results = []
+    if n_dices > 0:
+        for i in range(n_dices):
+            results.append(random.randint(1,6))
+        return results
+    else:
+        return ("No dices were rolled.")
+
+while True:
+    dice_roll(n_dices)    
+    input()
