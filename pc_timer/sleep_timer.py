e0cad6e9263da4449449b0572716cfbb36e3c617
Rharsia <Jan.Zhang@seznam.cz>
2023-04-12 09:18:14 +0200

    Expanded the timer to hours

--- pc_timer/sleep_timer.py
+++ pc_timer/sleep_timer.py
@@ -6,9 +6,12 @@ print()
 
 # get the number of minutes from the user
 minutes = float(input("Enter the number of minutes: "))
+minutes = 65
 
 # convert to seconds
 seconds = round(minutes * 60)
+
+hours = minutes/60
 
 # display the message
 print(f"The computer will sleep in {minutes} minutes.")
@@ -16,10 +19,11 @@ print(f"The computer will sleep in {minutes} minutes.")
 # loop through the countdown timer
 for i in range(seconds, 0, -1):
     # calculate the minutes and seconds remaining
-    mins, secs = divmod(i, 60)
+    hours, remainder = divmod(i, 3600)
+    mins, secs = divmod(remainder, 60)
 
     # display the remaining time
-    print(f"{mins:02d}:{secs:02d}", end="\r")
+    print(f"{hours:02d}:{mins:02d}:{secs:02d}", end="\r")
 
     # wait for one second
     time.sleep(1)
