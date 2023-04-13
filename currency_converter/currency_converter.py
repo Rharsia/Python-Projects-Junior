5a6ffe7840a12ab3098ca18e670a3173c453a0a1
Rharsia <Jan.Zhang@seznam.cz>
2023-04-13 22:26:35 +0200

    basic converter

--- currency_converter/currency_converter.py
+++ currency_converter/currency_converter.py
@@ -7,3 +7,17 @@ response = requests.get(url)
 data = response.json()
 rates = data["rates"]
 
+def convert_currency(amount, from_currency, to_currency):
+    if from_currency != "USD":
+        # convert from USD to currency
+        amount = amount / rates[from_currency]
+
+    return round(amount * rates[to_currency])
+
+amount = 447655
+from_currency = "CZK"
+to_currency = "USD"
+
+converted_amount = convert_currency(amount, from_currency, to_currency)
+
+print(f"{amount} {from_currency} is equal to {converted_amount} {to_currency}")
